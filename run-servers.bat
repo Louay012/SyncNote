e5f77a885566ea6f2c9bbe4eaaca24112bd8d5b3
louay <louayhamdeni00@gmail.com>
2026-03-31 14:40:48 +0100

    fix package.json

--- run-servers.bat
+++ run-servers.bat
@@ -20,10 +20,10 @@ call npm --prefix front install
 if errorlevel 1 goto :error
 
 echo [3/4] Starting backend server in a new terminal...
-start "SyncNote Backend" cmd /k "cd /d "%~dp0back" && npm run dev"
+start "SyncNote Backend" cmd /k "cd /d ""%~dp0back"" && npm run dev"
 
 echo [4/4] Starting frontend server in a new terminal...
-start "SyncNote Frontend" cmd /k "cd /d "%~dp0front" && npm run dev"
+start "SyncNote Frontend" cmd /k "cd /d ""%~dp0front"" && npm run dev"
 
 echo Servers launched.
 exit /b 0
